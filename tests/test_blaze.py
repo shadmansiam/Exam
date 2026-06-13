0f3241c02e9179fe815ec08fc5f5ecf3e28d58be
Moloy <moloy@gmail.com>
2026-06-13 11:47:03 +0600

    Add failure case for product price test in Blaze class

--- tests/test_blaze.py
+++ tests/test_blaze.py
@@ -11,4 +11,13 @@ def test_blaze_product_price(page):
 
     assert actual_price == expected_price
 
+def test_blaze_product_price_failure(page):
+    blaze_page=Blaze()
 
+    product_name = "HTC One M9"
+    expected_price = 800
+
+    blaze_page.open_site(page)
+    actual_price=blaze_page.get_product_price(page,product_name)
+
+    assert actual_price == expected_price
